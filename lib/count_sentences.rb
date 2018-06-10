f02b2004219e484c83733e1d958ac92423e03120
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-10 00:08:44 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,8 +2,11 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence?(sentence)
+    if sentence.end_with?(".")
+      returns true
+    end
+    return false
   end
 
   def question?
