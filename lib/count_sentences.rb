dbff88ac874816fde26d9566c40772f9b6a78823
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-10 22:51:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,22 +2,20 @@ require 'pry'
 
 class String
 
-  def sentence?(sentence)
-    if sentence.end_with?(".")
-      returns true
-    end
-    return false
+  def sentence?
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr = self.split(/[!?.]+/) #splits sentences (ending in . ? !) from each other and counts each sentence
+    arr.count
   end
 end
